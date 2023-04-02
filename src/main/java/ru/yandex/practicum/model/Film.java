576f6f96cb2b5061042ea0674a60ce0466eaecdc
Aleks <aleks312t@gmail.com>
2023-04-03 00:09:00 +0300

    -fix: Классы Film и User теперь заполнены

--- src/main/java/ru/yandex/practicum/model/Film.java
+++ src/main/java/ru/yandex/practicum/model/Film.java
@@ -1,6 +1,15 @@
 package ru.yandex.practicum.model;
 
-import lombok.*;
+import lombok.Data;
 
+import java.time.Instant;
+import java.util.Date;
+
+@Data
 public class Film {
+    int id;
+    String name;
+    String description;
+    Date releaseDate;
+    Instant duration;
 }
--- src/main/java/ru/yandex/practicum/model/User.java
+++ src/main/java/ru/yandex/practicum/model/User.java
@@ -1,6 +1,14 @@
 package ru.yandex.practicum.model;
 
-import lombok.*;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
 public class User {
+    int id;
+    String email;
+    String login;
+    String name;
+    Date birthday;
 }
